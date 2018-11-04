47ac759de68449ec78aaa16cadeac03ab3b36bdc
safiravanillia <32903307+safiravanillia@users.noreply.github.com>
2018-11-04 23:50:59 +0700

    sari versi user

--- ann.py
+++ ann.py
@@ -1,175 +1,134 @@
+from random import random
 import numpy as np
-#import matplotlib.pyplot as plt
 
-def print_network(net):
-    for i, layer in enumerate(net, 1):
-        print("Layer {} ".format(i))
-        for j, neuron in enumerate(layer, 1):
-            print("neuron {} :".format(j), neuron)
+# initialising a neural network
+def init_network(inputs, hid, outputs):
+    network = list()
+    hidden_layer = [{'weights': [random() for i in range(inputs + 1)]} for i in range(hid)]
+    network.append(hidden_layer)
+    output_layer = [{'weights': [random() for i in range(hid + 1)]} for i in range(outputs)]
+    network.append(output_layer)
+    return network
 
-
-def initialize_network(X,outputSize):
-    input_neurons = len(X[0])
-    hidden_neurons = input_neurons + 1
-    output_neurons = outputSize
-
-    n_hidden_layers = 1
-
-    net = list()
-
-    for h in range(n_hidden_layers):
-        if h != 0:
-            input_neurons = len(net[-1])
-
-        hidden_layer = [{'weights': np.random.uniform(size=input_neurons)} for i in range(hidden_neurons)]
-        net.append(hidden_layer)
-
-    output_layer = [{'weights': np.random.uniform(size=hidden_neurons)} for i in range(output_neurons)]
-    net.append(output_layer)
-
-    return net
-
-def activate_sigmoid(sum):
-    return (1 / (1 + np.exp(-sum)))
+def activate(weights, inputs):
+    activation = weights[-1]  # Added the neuron bias beforehand. -1 is used for last element of an array.
+    for i in range(len(weights) - 1):  # Taking only first two weights as third one is the bias, which has been alredy added.
+        activation += weights[i] * inputs[i]  # Calculating the activation of the neuron
+    return activation - threshold
 
 
-def forward_propagation(net, input):
-    row = input
-    for layer in net:
-        prev_input = np.array([])
+def transfer(activation):
+    if activation >= 0:
+        return 1
+    else: return 0
+
+def forward_prop(network,row):  # Forward Propagation....calculating the transfer activity of neuron and passing it to the neurons in next layer.
+    inputs = row
+    for layer in network:
+        new_inputs = []
         for neuron in layer:
-            sum = neuron['weights'].T.dot(row)
-
-            result = activate_sigmoid(sum)
-            neuron['result'] = result
-
-            prev_input = np.append(prev_input, [result])
-        row = prev_input
-
-    return row
+            activation = activate(neuron['weights'], inputs)
+            neuron['output'] = transfer(activation)
+            new_inputs.append(neuron['output'])
+        inputs = new_inputs
+    return inputs
 
 
-def sigmoidDerivative(output):
-    return output * (1.0 - output)
+# network = init_network(2,1,2)
+# row = [1,0,None]
+# output = forward_prop(network, row)
+# print(output)
+
+def transfer_der(output):
+    return output * (1 - output)
 
 
-def back_propagation(net, row, expected):
-    for i in reversed(range(len(net))):
-        layer = net[i]
-        errors = np.array([])
-        if i == len(net) - 1:
-            results = [neuron['result'] for neuron in layer]
-            errors = expected - np.array(results)
+def backward_prop_error(network, expected):
+    for i in reversed(range(len(network))):
+        layer = network[i]
+        errors = list()
+        if i != len(network) - 1:
+            for j in range(len(layer)):
+                error = 0.0
+                for neuron in network[i + 1]:
+                    error += (neuron['weights'][j] * neuron['delta'])
+                errors.append(error)
         else:
             for j in range(len(layer)):
-                herror = 0
-                nextlayer = net[i + 1]
-                for neuron in nextlayer:
-                    herror += (neuron['weights'][j] * neuron['delta'])
-                errors = np.append(errors, [herror])
-
+                neuron = layer[j]
+                errors.append(expected[j] - neuron['output'])
         for j in range(len(layer)):
             neuron = layer[j]
-            neuron['delta'] = errors[j] * sigmoidDerivative(neuron['result'])
+            neuron['delta'] = errors[j] * transfer_der(neuron['output'])
 
 
-def updateWeights(net, input, lrate):
-    for i in range(len(net)):
-        inputs = input
+def update_weights(network, row, l_rate):
+    for i in range(len(network)):
+        inputs = row[:-1]
         if i != 0:
-            inputs = [neuron['result'] for neuron in net[i - 1]]
-
-        for neuron in net[i]:
+            inputs = [neuron['output'] for neuron in network[i - 1]]  #
+        for neuron in network[i]:
             for j in range(len(inputs)):
-                neuron['weights'][j] += lrate * neuron['delta'] * inputs[j]
+                neuron['weights'][j] += l_rate * neuron['delta'] * inputs[j]  # Updating the weights of a neuron.
+            neuron['weights'][-1] += l_rate * neuron['delta']  # Updating the bias of a neuron
 
+# Make a prediction with a network
+def predict(network, row):
+ outputs = forward_prop(network, row)
+ return outputs.index(max(outputs))
 
-def training(net, epochs, lrate, n_outputs):
-    errors = []
-    for epoch in range(epochs):
+def train_network(network, train, l_rate, n_outputs):
+    n_epoch = 999
+    for epoch in range(n_epoch):
         sum_error = 0
-        for i, row in enumerate(X):
-            outputs = forward_propagation(net, row)
+        for row in train:
+            outputs = forward_prop(network, row)
+            expected = [0 for i in range(n_outputs)]
+            expected[row[-1]] = 1
+            sum_error += sum([(expected[i] - outputs[i]) ** 2 for i in range(len(expected))])
+            backward_prop_error(network, expected)
+            update_weights(network, row, l_rate)
+        print('>epoch=%d, lrate=%.3f, error=%.3f' % (epoch+1, l_rate, sum_error))
+        for row in dataset:
+            prediction = predict(network, row)
+            print('Expected=%d, Got=%d' % (row[-1], prediction))
+        if sum_error == 0 :
+            break
 
-            expected = [0.0 for i in range(n_outputs)]
-            expected[y[i]] = 1
-
-            sum_error += sum([(expected[j] - outputs[j]) ** 2 for j in range(len(expected))])
-            back_propagation(net, row, expected)
-            updateWeights(net, row, 0.05)
-        if epoch :
-            print('>epoch=%d,error=%.3f' % (epoch, sum_error))
-            errors.append(sum_error)
-    return errors
-
-
-inpdata = input("Pilih \n1.XOR 2.OR 3.AND 4.ParityBit :")
+inpdata = input("Pilih \n1.XOR 2.OR 3.AND 4.ParityBit : ")
 inpdata = int(inpdata)
-inputSize = input("Panjang Input : ")
-inputSize = int(inputSize)
-inx = input("Jumlah Input : ")
-inx = int(inx)
-inputx = []
-inx2 = 0
-
-for i in range(inx):
-    for j in range(inputSize):
-        print("[", i, "][", j, "] = ")
-        inputx.append(int(input(inx2)))
-
-#inputx=np.array(inputx)
-print(inputx)
-
 outputSize = input("Jumlah Output : ")
 outputSize = int(outputSize)
 
-lrate=input("Learning Rate : ")
+lrate = input("Learning Rate : ")
 lrate = float(lrate)
 
 if inpdata == 1:
     # Xor data
-    XORdata = np.array([inputx])
-    X = XORdata[:, 0:2]
-    y = XORdata[:, -1]
-    net = initialize_network(X,outputSize)
-    print_network(net)
-    errors = training(net, 10, lrate, outputSize)
+    dataset = [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0]]
+    n_inputs = len(dataset[0]) - 1  # Third value in the dataset entry is for biases.
+    n_outputs = len(set(row[-1] for row in dataset))
 
 elif inpdata == 2:
     # or data
-    ORdata = np.array([inputx])
-    X = ORdata[:, 0:2]
-    y = ORdata[:, -1]
-    net = initialize_network(X,outputSize)
-    print_network(net)
-    errors = training(net, 10, lrate, outputSize)
+    dataset = [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 1]]
+    n_inputs = len(dataset[0]) - 1  # Third value in the dataset entry is for biases.
+    n_outputs = len(set(row[-1] for row in dataset))
 
 elif inpdata == 3:
     # and data [[0,0,0],[0,1,0],[1,0,0],[1,1,1]]
-    ANDdata = np.array([inputx])
-    X = ANDdata[:, 0:2]
-    y = ANDdata[:, -1]
-    net = initialize_network(X,outputSize)
-    print_network(net)
-    errors = training(net, 10, lrate, outputSize)
+    dataset = [[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
+    n_inputs = len(dataset[0]) - 1  # Third value in the dataset entry is for biases.
+    n_outputs = len(set(row[-1] for row in dataset))
 
 elif inpdata == 4:
-    PARdata = np.array([inputx])
-    X = PARdata[:, 0:2]
-    y = PARdata[:, -1]
-    net = initialize_network(X,outputSize)
-    print_network(net)
-    errors = training(net, 10, lrate, outputSize)
+    dataset = [[0, 0, 0], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
 
 
-# Make a prediction with a network
-def predict(network, row):
-    outputs = forward_propagation(net, row)
-    return outputs
+hidden_l = int(input('Jumlah hidden Layer : '))
+threshold = float(input('Threshold : '))
+network = init_network(n_inputs, hidden_l, n_outputs)
+train_network(network, dataset, lrate, n_outputs)
 
-
-pred = predict(net, np.array([1, 0]))
-output = np.argmax(pred)
-print(output)
-
-print_network(net)
+for layer in network:
+    print(layer)
